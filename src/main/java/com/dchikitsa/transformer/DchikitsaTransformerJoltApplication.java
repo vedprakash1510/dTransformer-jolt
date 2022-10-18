801d5c8a0ff6a14f4bef4471c706b4b96fe5498d
vedprakash1510 <87705515+vedprakash1510@users.noreply.github.com>
2022-10-18 18:07:34 +0530

    Add files via upload

---
+++ src/main/java/com/dchikitsa/transformer/DchikitsaTransformerJoltApplication.java
@@ -0,0 +1,13 @@
+package com.dchikitsa.transformer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DchikitsaTransformerJoltApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DchikitsaTransformerJoltApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dchikitsa/transformer/DchikitsaTransformerJoltApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dchikitsa.transformer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DchikitsaTransformerJoltApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
